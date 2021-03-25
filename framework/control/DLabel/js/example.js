a2c69beb0c6be4a3c11c9744102b8955c5b95de1
Volant Lee <volant@163.com>
2021-03-25 17:54:58 +0800

    2021-03-25

--- framework/control/DLabel/js/example.js
+++ framework/control/DLabel/js/example.js
@@ -1,14 +1,168 @@
 ﻿var DLbelExample = {};
 
+// -- 空白新元素模板 -------------------------------------------------------------
+DLbelExample.getElement = function (elementType = "text", elementProp = {}) {
+    let element;
+    if (elementType.equals("text")) {
+        element = DLbelExample._getElement_text(elementProp);
+    }
+    else if (elementType.equals("barcode1D")) {
+        element = DLbelExample._getElement_barcode1D(elementProp);
+    }
+    else if (elementType.equals("barcode2D")) {
+        element = DLbelExample._getElement_barcode2D(elementProp);
+    }
+    else if (elementType.equals("image")) {
+        element = DLbelExample._getElement_image(elementProp);
+    }
+    else if (elementType.equals("shape")) {
+        element = DLbelExample._getElement_shape(elementProp);
+    }
+    else {
+        alert("debug here");
+    }
+    return element;
+}
+
+DLbelExample._getElement_text = function (elementProp = {}) {
+    let element = {
+        head: {
+            elementType: "text"
+        },
+        sections: [
+            UtilElement.getFixedSection({ pos: 0 }),
+            UtilElement.getBlankSection({ pos: 1 })
+        ],
+        font: {
+            // -- 宋体五号 --
+            name: "宋体",
+            size: "10.5"
+        },
+        frame: {},
+        position: {
+            "layer": 1,
+            width: 20,
+            height: 8,
+            textAlign: "left",
+            verticalAlign: "middle"
+        }
+    }
+
+    return element;
+}
+DLbelExample._getElement_barcode1D = function (elementProp = {}) {
+    let element = {
+        head: {
+            elementType: "barcode",
+            barcodeType: "CODE_128"
+        },
+        sections: [
+            UtilElement.getFixedSection({ pos: 0 }),
+            UtilElement.getBlankSection({ pos: 1 })
+        ],
+        font: {
+            // -- 宋体五号 --
+            name: "宋体",
+            size: "10.5"
+        },
+        frame: {},
+        position: {
+            "layer": 1,
+            width: 30,
+            height: 10,
+            textAlign: "center",
+            verticalAlign: "bottom"
+        }
+    }
+
+    return element;
+}
+DLbelExample._getElement_barcode2D = function (elementProp = {}) {
+    let element = {
+        head: {
+            elementType: "barcode",
+            barcodeType: "QR_CODE"
+        },
+        sections: [
+            UtilElement.getFixedSection({ pos: 0 }),
+            UtilElement.getBlankSection({ pos: 1 })
+        ],
+        font: {
+            // -- 宋体五号 --
+            name: "宋体",
+            size: "10.5"
+        },
+        frame: {},
+        position: {
+            "layer": 1,
+            width: 20,
+            height: 20,
+            textAlign: "center",
+            verticalAlign: "bottom"
+        }
+    }
+
+    return element;
+}
+
+DLbelExample._getElement_image = function (elementProp = {}) {
+    let element = {
+        head: {
+            elementType: "image"
+        },
+        frame: {},
+        position: {
+            "layer": 1,
+            width: 20,
+            height: 20,
+            textAlign: "center",
+            verticalAlign: "middle"
+        },
+        image: {
+            "value": "",
+            "deformation": "zoom",
+            "url": "http://biosunmed.com/upload/2020/7/61552460.jpg"
+        }
+    }
+
+    return element;
+}
+DLbelExample._getElement_shape = function (elementProp = {}) {
+    let element = {
+        head: {
+            elementType: "shape"
+        },
+        frame: {
+            "type": "rectangle",
+            "width": 0.5,
+            "color": "#000000"
+        },
+        position: {
+            "layer": 1,
+            width: 32,
+            height: 18,
+            textAlign: "center",
+            verticalAlign: "middle"
+        }
+    }
+
+    return element;
+}
+
+
+// -- 样例标签(演示、测试、调试用)、样例脚本 -----------------------------------------
 DLbelExample.getExample = function (labelKey) {
-    if (labelKey.equals("debug1")) {
+    if (labelKey.equals("demo1")) {
+        return DLbelExample.demo1();
+    }
+    else if (labelKey.equals("debug1")) {
         return DLbelExample.debug1();
     }
     else {
         return "";
     }
 }
-DLbelExample.debug1 = function () {
+DLbelExample.demo1 = function () {
     return `
 {
   "page": {
@@ -686,6 +840,197 @@ DLbelExample.debug1 = function () {
 }
         `;
 }
+DLbelExample.debug1 = function () {
+    return `
+{
+  "page": {
+    "width": 200,
+    "height": 150,
+    "marginLeft": 0,
+    "marginTop": 0,
+    "marginRight": 0,
+    "marginBottom": 0,
+    "rows": 2,
+    "cols": 2,
+    "horizontalSpace": 0,
+    "verticalSpace": 0
+  },
+  "head": {
+    "width": 80,
+    "height": 60,
+    "point": 600,
+    "imageBaseUrl": "",
+    "scriptAfterCompute": "",
+    "element_id": 9
+  },
+  "fields": {},
+  "elements": [
+    {
+      "head": {
+        "elementType": "shape",
+        "name": "element_8"
+      },
+      "frame": {
+        "type": "rectangle",
+        "width": 0.5,
+        "color": "#F42929"
+      },
+      "position": {
+        "layer": "0",
+        "width": "78",
+        "height": "58",
+        "textAlign": "center",
+        "verticalAlign": "middle",
+        "top": "1",
+        "left": "1",
+        "angle": 0,
+        "angleR": 0,
+        "marginLeft": 0,
+        "marginRight": 0,
+        "marginTop": 0,
+        "marginBottom": 0,
+        "wC": 78,
+        "hC": 58,
+        "P1": {
+          "x": 0,
+          "y": 0
+        },
+        "P2": {
+          "x": 78,
+          "y": 0
+        },
+        "P3": {
+          "x": 78,
+          "y": 58
+        },
+        "P4": {
+          "x": 0,
+          "y": 58
+        },
+        "P15": {
+          "x": 39,
+          "y": 0
+        },
+        "P25": {
+          "x": 78,
+          "y": 29
+        },
+        "P35": {
+          "x": 39,
+          "y": 58
+        },
+        "P45": {
+          "x": 0,
+          "y": 29
+        },
+        "offsetX": 0,
+        "offsetY": 0,
+        "offsetLeft": 0.5,
+        "offsetRight": 0.5,
+        "offsetTop": 0.5,
+        "offsetBottom": 0.5,
+        "clientWidth": 77,
+        "clientHeight": 57
+      },
+      "env": "design"
+    },
+    {
+      "head": {
+        "name": "element_1",
+        "elementType": "text"
+      },
+      "sections": [
+        {
+          "pos": 0,
+          "type": "fixed",
+          "value": "样张水印",
+          "format": ""
+        },
+        {
+          "pos": 1,
+          "type": "",
+          "value": "",
+          "format": ""
+        }
+      ],
+      "font": {
+        "lineHeight": 0,
+        "size": 12,
+        "name": "楷体",
+        "color": "#00FF48",
+        "bold": true
+      },
+      "frame": {
+        "type": "rectangle",
+        "width": 0.3,
+        "color": "#FFD000"
+      },
+      "position": {
+        "layer": 1,
+        "top": "20",
+        "left": "20",
+        "width": "25",
+        "height": "8",
+        "angle": "330",
+        "angleR": 5.759586531581287,
+        "textAlign": "center",
+        "verticalAlign": "middle",
+        "marginLeft": 0,
+        "marginRight": 0,
+        "marginTop": 0,
+        "marginBottom": 0,
+        "wC": 25.650635094610962,
+        "hC": 19.428203230275518,
+        "P1": {
+          "x": 0,
+          "y": 12.50000000000001
+        },
+        "P2": {
+          "x": 21.65063509461096,
+          "y": 0
+        },
+        "P3": {
+          "x": 25.650635094610962,
+          "y": 6.928203230275507
+        },
+        "P4": {
+          "x": 4.0000000000000036,
+          "y": 19.428203230275518
+        },
+        "P15": {
+          "x": 10.82531754730548,
+          "y": 6.250000000000005
+        },
+        "P25": {
+          "x": 23.650635094610962,
+          "y": 3.4641016151377535
+        },
+        "P35": {
+          "x": 14.825317547305483,
+          "y": 13.178203230275512
+        },
+        "P45": {
+          "x": 2.0000000000000018,
+          "y": 15.964101615137764
+        },
+        "offsetX": 0,
+        "offsetY": 12.50000000000001,
+        "leftText": 12.5,
+        "topText": 4,
+        "offsetLeft": 0.3,
+        "offsetRight": 0.3,
+        "offsetTop": 0.3,
+        "offsetBottom": 0.3,
+        "clientWidth": 24.4,
+        "clientHeight": 7.4
+      },
+      "env": "design",
+      "point": 600
+    }
+  ]
+}
+        `;
+}
 
 DLbelExample.getExampleScript = function (scriptKey) {
     let script;
@@ -715,153 +1060,4 @@ DLbelExample.script1 = function () {
         element.image.url = imgUrl;
     }
         `;
-}
-
-// -- 空白新元素模板 -------------------------------------------------------------
-DLbelExample.getElement = function (elementType = "text", elementProp = {}) {
-    let element;
-    if (elementType.equals("text")) {
-        element = DLbelExample._getElement_text(elementProp);
-    }
-    else if (elementType.equals("barcode1D")) {
-        element = DLbelExample._getElement_barcode1D(elementProp);
-    }
-    else if (elementType.equals("barcode2D")) {
-        element = DLbelExample._getElement_barcode2D(elementProp);
-    }
-    else if (elementType.equals("image")) {
-        element = DLbelExample._getElement_image(elementProp);
-    }
-    else if (elementType.equals("sharp")) {
-        element = DLbelExample._getElement_sharp(elementProp);
-    }
-    else {
-        alert("debug here");
-    }
-    return element;
-}
-
-DLbelExample._getElement_text = function (elementProp = {}) {
-    let element = {
-        head: {
-            elementType: "text"
-        },
-        sections: [
-            UtilElement.getFixedSection({ pos: 0 }),
-            UtilElement.getBlankSection({ pos: 1 })
-        ],
-        font: {
-            // -- 宋体五号 --
-            name: "宋体",
-            size: "10.5"
-        },
-        frame: {},
-        position: {
-            "layer": 1,
-            width: 20,
-            height: 8,
-            textAlign: "left",
-            verticalAlign: "middle"
-        }
-    }
-
-    return element;
-}
-DLbelExample._getElement_barcode1D = function (elementProp = {}) {
-    let element = {
-        head: {
-            elementType: "barcode",
-            barcodeType: "CODE_128"
-        },
-        sections: [
-            UtilElement.getFixedSection({ pos: 0 }),
-            UtilElement.getBlankSection({ pos: 1 })
-        ],
-        font: {
-            // -- 宋体五号 --
-            name: "宋体",
-            size: "10.5"
-        },
-        frame: {},
-        position: {
-            "layer": 1,
-            width: 30,
-            height: 10,
-            textAlign: "center",
-            verticalAlign: "bottom"
-        }
-    }
-
-    return element;
-}
-DLbelExample._getElement_barcode2D = function (elementProp = {}) {
-    let element = {
-        head: {
-            elementType: "barcode",
-            barcodeType: "QR_CODE"
-        },
-        sections: [
-            UtilElement.getFixedSection({ pos: 0 }),
-            UtilElement.getBlankSection({ pos: 1 })
-        ],
-        font: {
-            // -- 宋体五号 --
-            name: "宋体",
-            size: "10.5"
-        },
-        frame: {},
-        position: {
-            "layer": 1,
-            width: 20,
-            height: 20,
-            textAlign: "center",
-            verticalAlign: "bottom"
-        }
-    }
-
-    return element;
-}
-
-DLbelExample._getElement_image = function (elementProp = {}) {
-    let element = {
-        head: {
-            elementType: "image"
-        },
-        frame: {},
-        position: {
-            "layer": 1,
-            width: 20,
-            height: 20,
-            textAlign: "center",
-            verticalAlign: "middle"
-        },
-        image: {
-            "value": "",
-            "deformation": "zoom",
-            "url": "http://biosunmed.com/upload/2020/7/61552460.jpg"
-        }
-    }
-
-    return element;
-}
-DLbelExample._getElement_sharp = function (elementProp = {}) {
-    let element = {
-        head: {
-            elementType: "sharp"
-        },
-        frame: {
-            "type": "rectangle",
-            "width": 0.5,
-            "color": "#000000"
-        },
-        position: {
-            "layer": 1,
-            width: 32,
-            height: 18,
-            textAlign: "center",
-            verticalAlign: "middle"
-        }
-    }
-
-    return element;
 }
