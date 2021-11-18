29ae7f1b70fab8ecbb0289966f9c8c9f5e7d701e
Volant Lee <volant@163.com>
2021-11-18 16:52:52 +0800

    2021-11-18

--- framework/control/DLabel/js/doys.js
+++ framework/control/DLabel/js/doys.js
@@ -8,7 +8,7 @@
  */
 
 (function () {
-    window.jsVer = top.jsVer || "2021-09-14";
+    window.jsVer = top.jsVer || "2021-01-18";
     if (document.documentURI.indexOf("//127.0.0.1/") > 0) {
         window.jsVer = (new Date()).getTime();
     }
